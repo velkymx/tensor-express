089374bd04a71c76ed810febbba3af939c3fe54e
AJ <velkymx@gmail.com>
2023-03-08 16:15:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Set up static file serving
+app.use(express.static('public'));
+
+// Set up view engine
+app.set('view engine', 'ejs');
+
+// Set up routes
+const apiRouter = require('./routes/api');
+app.use('/api', apiRouter);
+
+// Set up server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
